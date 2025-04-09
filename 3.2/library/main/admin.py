b5490c5749b8679f6729301a7a4b087dbeb8e508
MaxMishaev <maxmishaev74@gmail.com>
2025-04-09 20:21:52 +0300

    3.2_CRUD_Commit

--- 3.2/library/main/admin.py
+++ 3.2/library/main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from main.models import Book, Order
+from .models import Book, Order
 
 
 admin.site.register(Book)
--- 3.2/library/main/serializers.py
+++ 3.2/library/main/serializers.py
@@ -1,23 +1,29 @@
 from rest_framework import serializers
+from .models import Book, Order
+
 
 
 class BookSerializer(serializers.ModelSerializer):
     # реализуйте сериализацию объектов модели Book
-    ...
+    class Meta:
+        model = Book
+        fields = ['id', 'author', 'title', 'year']
 
-    #доп задание
-    # def to_representation(self, instance):
-    #     representation = super().to_representation(instance)
-    #     representation['orders_count'] = ...
-    #     return representation
+    #доп задание (пока не получилось :-( )
+    #def to_representation(self, instance):
+    #    data = super().to_representation(instance)
+    #    data['orders_count'] = instance.author
+    #    return data
 
 
 class OrderSerializer(serializers.ModelSerializer):
     # добавьте поля модели Order
-    ...
+    class Meta:
+        model = Order
+        fields = ['id', 'user_name', 'days_count', 'date', 'books']
 
     #доп задание
-    # def to_representation(self, instance):
-    #     representation = super().to_representation(instance)
-    #     representation['books'] = ...
-    #     return representation
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['books'] = [{'author': book.author, 'title': book.title, 'year': book.year} for book in instance.books.all()]
+        return data
